9829211656c37c1166e12fc6b331f2ff4aa5f7ee
sirsasha <krypn87@mail.ru>
2026-03-18 00:26:53 +0800

    fix(app_run): add serialiser.is_valid

--- app_run/views.py
+++ app_run/views.py
@@ -626,4 +626,7 @@ class AnalyticsForCoachView(APIView):
             }
 
         serializer = AnalyticsSerializer(data_analytics)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        if serializer.is_valid(raise_exception=True):
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
